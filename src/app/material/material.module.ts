a51dae8196a98c1055191065c58e0c047dae14ce
karenyulier <karenyulierr@gmail.com>
2024-10-24 01:10:30 -0500

    structure app

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+@NgModule({
+  declarations: [
+    Error404PageComponent
+  ],
+  imports: [
+  ]
+})
+export class MaterialModule { }
