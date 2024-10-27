6cb5300123cf386cb20988e3fbcfe6cd0df66aec
karenyulier <karenyulierr@gmail.com>
2024-10-26 19:56:20 -0500

    add guard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -8,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor(private authService:AuthService, private router:Router){}
+
+  onLogin():void{
+    this.authService.login("karen", "12345")
+    .subscribe(user =>{
+      this.router.navigate(['/'])
+    });
+  }
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -8,11 +11,21 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
   public sidebarItems =
     [
-      { label: 'Listado', icon: 'label', url:'./list' },
-      { label: 'Añadir', icon: 'add', url:'./new-hero' },
-      { label: 'Buscar', icon: 'search', url:'./search' }
-    ]
+      { label: 'Listado', icon: 'label', url: './list' },
+      { label: 'Añadir', icon: 'add', url: './new-hero' },
+      { label: 'Buscar', icon: 'search', url: './search' }
+    ];
+
+  get User(): User | undefined {
+    return this.authService.currentUser;
+
+  }
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 
 }
